b4df5032af1e9be636b139502488f791a66c9cf0
Ubuntu <ubuntu@ip-172-31-1-91.us-west-2.compute.internal>
2013-07-09 20:06:15 +0000

    Install express and update web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var index = new Buffer(fs.readFileSync('index.html'),"utf=8"); 
+ response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
